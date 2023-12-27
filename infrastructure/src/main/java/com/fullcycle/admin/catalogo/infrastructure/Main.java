d488c0cabbd9383f8e352e123b3fa95db287a3a0
Lucas Ramos <lucasramosgab90@gmail.com>
2023-12-27 17:29:33 -0300

    Adicionando o springboot na camada de infrastructure

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,10 +1,13 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fullcycle.admin.catalogo.application.UseCase;
-
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Hello world");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
