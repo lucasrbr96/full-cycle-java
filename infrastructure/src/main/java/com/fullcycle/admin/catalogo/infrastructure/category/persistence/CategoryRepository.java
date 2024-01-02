979b016b31b0529131015735e572c718440d4ec1
Lucas Ramos <lucasramosgab90@gmail.com>
2024-01-02 19:59:35 -0300

    Criando o jpa repository da categoria

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/persistence/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.fullcycle.admin.catalogo.infrastructure.category.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<CategoryJpaEntity, String> {
+}
