1b3f05182fefa0e7ebfd1ef5d95a93f11939a0b6
Lucas Ramos <lucasramosgab90@gmail.com>
2024-01-03 18:12:05 -0300

    Criando a estrategia para testes integrados de banco de dados

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -0,0 +1,24 @@
+package com.fullcycle.admin.catalogo.infrastructure.category;
+
+import com.fullcycle.admin.catalogo.infrastructure.MySQLGatewayTest;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@MySQLGatewayTest
+class CategoryMySQLGatewayTest {
+
+    @Autowired
+    private CategoryMySQLGateway categoryGateway;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void testInjectedDependencies(){
+        Assertions.assertNotNull(categoryGateway);
+        Assertions.assertNotNull(categoryRepository);
+    }
+
+}
