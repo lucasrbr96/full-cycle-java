b6407a45c0172ecab5ff6e36a672997912cb3bd0
Lucas Ramos <lucasramosgab90@gmail.com>
2024-02-01 19:28:42 -0300

    Testes integrados do endpoint de criação de categoria

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -1,11 +1,35 @@
 package com.fullcycle.admin.catalogo.infrastructure.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fullcycle.admin.catalogo.ControllerTest;
+import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryCommand;
+import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryOutput;
 import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
+import io.vavr.API;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Objects;
+
+import static io.vavr.API.Right;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ControllerTest(controllers = CategoryAPI.class)
 class CategoryAPITest {
@@ -13,10 +37,40 @@ class CategoryAPITest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @MockBean
     private CreateCategoryUseCase createCategoryUseCase;
-    @Test
-    void test(){
 
+    @Test
+    void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception{
+
+        final var expectedName = "filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var aInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenReturn(Right(CreateCategoryOutput.from(CategoryID.from("123"))));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(aInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpectAll(
+                        status().isCreated(),
+                        header().string("Location", "/categories/123")
+                );
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                        Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+                ));
     }
 }
