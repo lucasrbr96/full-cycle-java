5795bacc12092a3765356437e0913e9cbc171c2d
Lucas Ramos <lucasramosgab90@gmail.com>
2024-02-25 13:35:42 -0300

    Criando a estrategia para testes e2e com testcontainers

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/category/CategoryE2ETest.java
@@ -0,0 +1,37 @@
+package com.fullcycle.admin.catalogo.e2e.category;
+
+import com.fullcycle.admin.catalogo.E2ETest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@E2ETest
+@Testcontainers
+public class CategoryE2ETest {
+
+    @Container
+    private static final MySQLContainer MY_SQL_CONTAINER
+            = new MySQLContainer("mysql:latest")
+                .withPassword("123456")
+                .withUsername("root")
+                .withDatabaseName("adm_videos");
+
+    @DynamicPropertySource
+    public static void setDataSourceProperties(final DynamicPropertyRegistry registry){
+        final var mappedPort = MY_SQL_CONTAINER.getMappedPort(3306);
+        System.out.printf("container is running on port %s\n", mappedPort);
+        registry.add("mysql.port", () -> mappedPort);
+        System.setProperty("spring.datasource.url", MY_SQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", MY_SQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", MY_SQL_CONTAINER.getPassword());
+    }
+
+    @Test
+    public void testWorks(){
+        Assertions.assertTrue(MY_SQL_CONTAINER.isRunning());
+    }
+}
