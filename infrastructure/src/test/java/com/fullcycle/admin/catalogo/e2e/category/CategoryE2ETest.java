a5d7209f904d619196191e0bfbb969d2e4dd16d8
Lucas Ramos <lucasramosgab90@gmail.com>
2024-02-29 17:47:57 -0300

    Testes e2e da listagem de categoria

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/e2e/category/CategoryE2ETest.java
@@ -6,6 +6,7 @@ import com.fullcycle.admin.catalogo.infrastructure.category.models.CategoryRespo
 import com.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryRequest;
 import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.json.Json;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @E2ETest
@@ -80,6 +82,111 @@ public class CategoryE2ETest {
                 .getResponse().getContentAsString();
 
         return Json.readValue(json, CategoryResponse.class);
+    }
+
+    @Test
+    public void asACatalogAdminIShouldBeAbleToNavigateToPageAllCategories() throws Exception {
+        Assertions.assertTrue(MY_SQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        givenACategory("Filmes", null, true);
+        givenACategory("Documentários", null, true);
+        givenACategory("Séries", null, true);
+
+         listCategories(0, 1)
+                 .andExpect(status().isOk())
+                 .andExpect(jsonPath("$.current_page", Matchers.equalTo(0)))
+                 .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                 .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                 .andExpect(jsonPath("$.items", Matchers.hasSize(1)))
+                 .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Documentários")));
+
+        listCategories(1, 1)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Filmes")));
+
+        listCategories(2, 1)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(2)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Séries")));
+
+        listCategories(3, 1)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(0)));
+    }
+
+    @Test
+    public void asACatalogAdminIShouldBeAbleToSearchBetweenAllCategories() throws Exception {
+        Assertions.assertTrue(MY_SQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        givenACategory("Filmes", null, true);
+        givenACategory("Documentários", null, true);
+        givenACategory("Séries", null, true);
+
+        listCategories(0,1, "fil")
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(0)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(1)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Filmes")));
+
+    }
+
+    @Test
+    public void asACatalogAdminIShouldBeAbleToSortAllCategoriesByDescriptionDesc() throws Exception {
+        Assertions.assertTrue(MY_SQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        givenACategory("Filmes", "C", true);
+        givenACategory("Documentários", "Z", true);
+        givenACategory("Séries", "A", true);
+
+        listCategories(0,3, "", "description", "desc")
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.current_page", Matchers.equalTo(0)))
+                .andExpect(jsonPath("$.per_page", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.total", Matchers.equalTo(3)))
+                .andExpect(jsonPath("$.items", Matchers.hasSize(3)))
+                .andExpect(jsonPath("$.items[0].name", Matchers.equalTo("Documentários")))
+                .andExpect(jsonPath("$.items[1].name", Matchers.equalTo("Filmes")))
+                .andExpect(jsonPath("$.items[2].name", Matchers.equalTo("Séries")));
+
+    }
+
+
+    private ResultActions listCategories(final int page, final int perPage, final String search) throws Exception {
+        return listCategories(page, perPage, search, "", "");
+    }
+    private ResultActions listCategories(final int page, final int perPage) throws Exception {
+        return listCategories(page, perPage, "", "", "");
+    }
+
+        private ResultActions listCategories(final int page, final int perPage, final String search, final String sort, final String directions) throws Exception {
+        final var aRequest = get("/categories")
+                .queryParam("page", String.valueOf(page))
+                .queryParam("perPage", String.valueOf(perPage))
+                .queryParam("search", search)
+                .queryParam("sort", sort)
+                .queryParam("dir", directions)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        final var json = this.mvc.perform(aRequest)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse().getContentAsString();
+        return this.mvc.perform(aRequest);
     }
 
     private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
