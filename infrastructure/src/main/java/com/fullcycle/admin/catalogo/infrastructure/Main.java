ca2b0e6c8fff5d5ba0691f56a192d141541365cc
Lucas Ramos <lucasramosgab90@gmail.com>
2024-03-13 20:56:45 -0300

    Testes de regressão manual com postman

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -22,7 +22,7 @@ import java.util.List;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
