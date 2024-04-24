22736d527dfb6f580c18f0c51f22c58ebd1f23c5
Lucas Ramos <lucasramosgab90@gmail.com>
2024-04-24 18:56:06 -0300

    Criando a entidade jpa pivo genre categories

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/persistence/GenreCategoryID.java
@@ -0,0 +1,54 @@
+package com.fullcycle.admin.catalogo.infrastructure.genre.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+public class GenreCategoryID {
+
+    @Column(name = "genre_id", nullable = false)
+    private String genreId;
+    @Column(name = "category_id", nullable = false)
+    private String categoryId;
+
+    public GenreCategoryID(){}
+
+    private GenreCategoryID(final String aGenreId, final String aCategoryId){
+        this.categoryId = aCategoryId;
+        this.genreId = aGenreId;
+    }
+
+    public static GenreCategoryID from(final String aGenreId, final String aCategoryId){
+        return new GenreCategoryID(aGenreId, aCategoryId);
+    }
+
+    public String getGenreId() {
+        return genreId;
+    }
+
+    public void setGenreId(String genreId) {
+        this.genreId = genreId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final GenreCategoryID that = (GenreCategoryID) o;
+        return Objects.equals(genreId, that.genreId) && Objects.equals(categoryId, that.categoryId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(genreId, categoryId);
+    }
+}
