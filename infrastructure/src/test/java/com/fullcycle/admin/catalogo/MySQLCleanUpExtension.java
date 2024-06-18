9f50f85c6fe973aab6bbe206b9a8ff3ae3009db0
Lucas Ramos <lucasramosgab90@gmail.com>
2024-06-18 10:46:36 -0300

    criando o jpa repository de castmember

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/MySQLCleanUpExtension.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/MySQLCleanUpExtension.java
@@ -1,11 +1,10 @@
 package com.fullcycle.admin.catalogo;
 
+import com.fullcycle.admin.catalogo.infrastructure.castmember.persistence.CastMemberRepository;
 import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.fullcycle.admin.catalogo.infrastructure.genre.persistence.GenreRepository;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -19,6 +18,7 @@ public class MySQLCleanUpExtension implements BeforeEachCallback {
         final var appContext = SpringExtension.getApplicationContext(context);
 
         cleanUp(List.of(
+                appContext.getBean(CastMemberRepository.class),
                 appContext.getBean(GenreRepository.class),
                 appContext.getBean(CategoryRepository.class)
         ));
