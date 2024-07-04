d20c69d2025596da3ce566c907cb8c8bb7505fb2
Lucas Ramos <lucasramosgab90@gmail.com>
2024-07-04 10:55:52 -0300

    Test Api de castMember

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/exceptions/NotificationException.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/exceptions/NotificationException.java
@@ -9,4 +9,8 @@ public class NotificationException extends DomainException{
     public NotificationException(final String aMessage, final Notification notification) {
         super(aMessage, notification.getErrors());
     }
+
+    public static NotificationException with(Error error) {
+        return new NotificationException("", Notification.create(error));
+    }
 }
